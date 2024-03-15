827a41ece44df73746e849d53bac0235d7c1a521
zahra <shabrinazahra08@gmail.com>
2024-03-15 16:07:54 +0700

    First init github

---
+++ app/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Model
+{
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $KeyType = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class, "user_id", "id");
+    }
+
+    public function getAuthIdentifierName()
+    {
+        return 'username';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->username;
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'token';
+    }
+}
+
+
